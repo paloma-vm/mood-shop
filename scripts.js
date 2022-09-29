043b4ac71990f425f58a90fe6fed0acfa89f6702
PalomaV-M <112986916+PalomaV-M@users.noreply.github.com>
2022-09-28 23:23:19 -0600

    displayed cart items on console

--- scripts.js
+++ scripts.js
@@ -32,28 +32,69 @@ for (let i = 0; i < data.length; i += 1) {
 
 }
 
-const cart = [ ]
+const cart = []
 
-const obj = {name:'shoe', price:9.99, qty: 3}
-console.log(obj)
-console.log(obj.price * obj.price)
+// const a = 999
+// const obj = { a }
+// console.log(obj)
+// console.log('****************')
+
+// const obj = {name:'shoe', price:9.99, qty: 3}
+// console.log(obj)
+// console.log(obj.price * obj.price)
 
 function addItem(name, price) {
-    const item = {name: name, price: price, qty: 1}
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            cart[i].qty += 1
+            // stop here!
+            return 
+        }
+    }
+    // const item = {name: name, price: price, qty: 1}
+    const item = {name, price, qty: 1}
     cart.push(item)
     
 }
-
+// Show Items
 function showItems() {
+
     // console.log(cart)
     // console.log( cart[0] )
     // console.log(cart.length)
-    console.log(`You have ${cart.length} items in your cart`)
+    console.log(`You have ${getQty()} items in your cart`)
+    
+    for (let i = 0; i < cart.length; i += 1) {
+        console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+    }
+
+    console.log(`Total in cart: $${getTotal()}`)
+
+    // get Qty
+    function getQty() {
+        let qty = 0
+        for (let i = 0; i < cart.length; i += 1) {
+            qty += cart[i].qty
+        }
+        return qty
+    }
+    // Get total
+    function getTotal() {
+        let total = 0
+        for (let i = 0; i < cart.length; i += 1) {
+            total += cart[i].price * cart[i].qty
+        }
+        return total.toFixed(2)
+    }
+
 }
 
 addItem('Apple', 0.99)
 addItem('Orange', 1.29)
 addItem('Opinion', 0.02)
+addItem('Apple', 0.99)
 addItem('Frisbee', 9.92)
+addItem('Apple', 0.99)
+addItem('Orange', 1.29)
 
 showItems()
